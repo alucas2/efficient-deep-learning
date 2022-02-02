7b5f16111025edfd47cefd8a091c6aaa0864ec22
Anthony LUCAS <a19lucas@pc-elec-187.priv.enst-bretagne.fr>
2022-02-02 15:59:03 +0100

    initial commit

---
+++ lab1_test.py
@@ -0,0 +1,50 @@
+import torch
+import torchvision
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data.dataloader import DataLoader
+import numpy as np
+import matplotlib.pyplot as plt
+from minicifar import minicifar_test
+from lab1_model import ResNet, BasicBlock
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+resnet = ResNet(BasicBlock, [2, 2, 2, 2])
+resnet.to(device)
+
+PATH = './lab1_resnet.pth'
+resnet.load_state_dict(torch.load(PATH))
+
+testloader = DataLoader(minicifar_test,batch_size=32) 
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+correct = 0
+total = 0
+with torch.no_grad():  # torch.no_grad for TESTING
+    for data in testloader:
+        images, labels = data
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = resnet(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
+
+# print images
+# imshow(torchvision.utils.make_grid(images))
+# print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# plt.show()
---
+++ lab1_train.py
@@ -0,0 +1,47 @@
+from minicifar import minicifar_train,minicifar_test,train_sampler,valid_sampler
+from torch.utils.data.dataloader import DataLoader
+
+trainloader = DataLoader(minicifar_train,batch_size=32,sampler=train_sampler)
+validloader = DataLoader(minicifar_train,batch_size=32,sampler=valid_sampler)
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from lab1_model import ResNet, BasicBlock
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+resnet = ResNet(BasicBlock, [2, 2, 2, 2])
+resnet.to(device)
+
+optimizer = optim.SGD(resnet.parameters(), lr=0.01)
+criterion = nn.CrossEntropyLoss()
+n_epochs = 100
+
+for epoch in range(n_epochs):  # loop over the dataset multiple times
+
+    resnet.train()
+    running_loss = 0.0
+    for i, data in enumerate(trainloader):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = resnet(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i == 99:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+PATH = './lab1_resnet.pth'
+torch.save(resnet.state_dict(), PATH)
